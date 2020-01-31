4853c63f0991b4b94c3a78ac01f51022f05f104a
Doniyorbek <doniyorbek.mashrabboyev@gmail.com>
2020-01-31 17:39:34 +0500

    Update manage.py
    added test commit

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+# Hello World
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'madadkor.settings')
